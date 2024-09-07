ead2f8c0ee4c5995b53037b303e2ccae9c7f405b
terry-man-com <terry.man.com@icloud.com>
2024-09-07 17:56:14 +0900

    単純なあいさつ終了

---
+++ resources/views/greeting/simple.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>単純な挨拶</title>
+</head>
+<body>
+    <h1>{{ $t }}のあいさつ</h1>
+    <p>{{ $r }}</p>
+</body>
+</html>
